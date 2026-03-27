5eb395e2a9bbc4f711214d37f063d1161298bb68
Hyeonmin Seon <hmsun.es@esgroup.net>
2026-03-27 20:16:26 +0900

    feat: Vue 컴포넌트 및 기능 추가
    - App.vue에 버튼 및 입력 필드 추가
    - main.js에서 createApp 임포트 위치 수정

--- week3-2/src/todo-app/src/App.vue
+++ week3-2/src/todo-app/src/App.vue
@@ -1,4 +1,16 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const buttonName = ref('버튼123')
+
+function test() {
+  alert('haha')
+}
+
+console.log(buttonName.value)
+
+globalThis.test = buttonName
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -8,7 +20,9 @@
   </p>
 
   <div>
-    <Button>Button</Button>
+    <Button @click="test">{{ buttonName }}</Button> <br />
+    <InputText placeholder="Username" v-model="buttonName" /> <br />
+    <input type="text" placeholder="Username" :value="버튼12333" />
   </div>
 </template>
 
--- week3-2/src/todo-app/src/main.js
+++ week3-2/src/todo-app/src/main.js
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
 import App from './App.vue'
+import { createApp } from 'vue';
 
 const app = createApp(App);
 app.use(PrimeVue, {
