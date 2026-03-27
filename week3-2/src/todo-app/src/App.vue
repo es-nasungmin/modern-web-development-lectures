b132a3137efbe448c66e0473beeb83776d30c73e
ekslffh <ekslffh@gmail.com>
2026-03-27 19:40:13 +0900

    feat: PrimeVue 및 관련 패키지 추가 및 설정

--- week3-2/src/todo-app/src/App.vue
+++ week3-2/src/todo-app/src/App.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import { Button } from 'primevue'
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +8,10 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+
+  <div>
+    <Button>Button</Button>
+  </div>
 </template>
 
 <style scoped></style>
